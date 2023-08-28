7dd6b99994b29f5f14f177076deaaf98b7969bf2
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-08-28 00:09:30 -0400

    95% complete

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
+
+  const { id } = useParams();
+
+  console.log(channelDetail, videos);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight='95vh'>
+      <Box>
+        <div
+          style={{
+            background:
+              "radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+        <ChannelCard
+          channelDetail={channelDetail}
+          marginTop='-110px'
+        ></ChannelCard>
+      </Box>
+      <Box display='flex' p='2'>
+        <Box sx={{ mr: { sm: "100px" } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
+};
+
+export default ChannelDetail;
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "6cfa3baf4dmsh16403284bc90d15p1cc302jsn8cd92abeb9ad",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
